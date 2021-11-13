6dfe399002db9e6935a19236b81476e9e0031a17
OkhotnikovFN <okhotnikov.fedor@yandex.ru>
2021-11-13 12:35:05 +0300

    change algoritm in task b

--- trainings_1.0/hw_1/task_a/a.py
+++ trainings_1.0/hw_1/task_a/a.py
@@ -2,7 +2,7 @@ def determine_temperature(mode: str, t_room: int, t_cond: int) -> int:
     """
     Функция определения температуры в комнате.
         Параметры:
-            mode: режим работы кондиционера
+            mode(str): режим работы кондиционера
             t_room(int): температура в комнате
             t_cond(int): температура установленная на кондиционере
         Возвращаемое значение:
@@ -16,7 +16,7 @@ def determine_temperature(mode: str, t_room: int, t_cond: int) -> int:
 
 
 def main():
-    """Основная функция для чтения входных данных и вывода результата"""
+    """Основная функция для чтения входных данных и вывода результата."""
     t_room, t_cond = map(int, input().split())
     operating_mode = input()
     print(determine_temperature(operating_mode, t_room, t_cond))
