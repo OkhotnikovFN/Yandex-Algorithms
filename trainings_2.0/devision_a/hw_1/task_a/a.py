bad9aac40ba7325208d0eac1cae8102e0d32fce5
OkhotnikovFN <okhotnikov.fedor@yandex.ru>
2021-11-22 22:08:33 +0300

    add structure for training 2.0

---
+++ trainings_2.0/devision_a/hw_1/task_a/a.py
@@ -0,0 +1,21 @@
+def my_func():
+    """
+    Функция которая.
+
+    :param :
+    :type :
+
+    :return:
+    :rtype:
+    """
+
+    return
+
+
+def main():
+    """Основная функция для чтения входных данных и вывода результата."""
+    print(my_func())
+
+
+if __name__ == '__main__':
+    main()
